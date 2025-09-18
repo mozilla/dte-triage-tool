721d813615177450bac93e263bc805fb9c396a30
Ben Chatterton <153005063+ben-c-at-moz@users.noreply.github.com>
2025-09-18 08:31:56 -0700

    Update conftest.py
    prevent previous methods from deletion

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,9 +1,13 @@
 import pytest
+import json
+from pytest_httpserver import HTTPServer
+from pathlib import Path
 from unittest.mock import MagicMock
 
 from src.core.state import SessionState
 from src.core.triage import Triage
 
+SOURCE_LOC = "tests/unit"
 
 @pytest.fixture(autouse=True)
 def reset_singleton():
@@ -11,6 +15,8 @@ def reset_singleton():
     yield
     Triage._instance = None
 
+def get_payload(endpoint, payload_type):
+    return json.load(Path(SOURCE_LOC, f"{endpoint}_{payload_type}.json").open())
 
 @pytest.fixture
 def mock_tr_session(mocker):
@@ -19,6 +25,23 @@ def mock_tr_session(mocker):
     mock_tr.return_value = mock
     return mock
 
+@pytest.fixture()
+def mock_bugzilla(httpserver: HTTPServer):
+    httpserver.expect_request(
+        "/rest/bug",
+        query_string={"api_key": "abcdefghijklmnop"},
+        json=get_payload("create", "request"),
+        method="POST",
+    ).respond_with_json(get_payload("create", "response"))
+    httpserver.expect_request(
+        "/rest/bug",
+        query_string={"api_key": "abcdefghijklmnop"},
+        json=get_payload("update", "request"),
+        method="PUT",
+    ).respond_with_json(get_payload("update", "response"))
+    httpserver.expect_request("/rest/bug")
+    yield httpserver
+    
 @pytest.fixture
 def session_state():
     return SessionState({"state": True})
